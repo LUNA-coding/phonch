5254fffba01096d9d36d9a58952a7671d5d71f43
hayward2007 <hayward2007@naver.com>
2024-08-10 04:13:56 +0900

    feat: home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Text, Touchable, TouchableOpacity, View} from 'react-native';
+
+import Change from '../../../assets/icons/autorenew.svg';
+import Account from '../../../assets/icons/account_circle.svg';
 
 import {styles} from './style';
+import {AppColor} from '../../global';
 
 export default function Home(): React.JSX.Element {
+  const [rank, setRank] = useState("Today's");
+
   return (
     <>
       <View style={styles.ranking}>
+        <View style={styles.rankingTitle}>
+          {/* total / today rank change button */}
+          <TouchableOpacity>
+            <Change width={28} height={28} fill={AppColor.brand_white} />
+          </TouchableOpacity>
+          <Text style={styles.rankingTitleText}>{rank} Ranking</Text>
+          <TouchableOpacity>
+            <Account width={28} height={28} fill={AppColor.brand_white} />
+          </TouchableOpacity>
+        </View>
         <Text>asdfasdf</Text>
       </View>
       <View style={styles.boxing}>
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -1,6 +1,17 @@
 import {StyleSheet} from 'react-native';
+import {AppColor} from '../../global';
 
 export const styles = StyleSheet.create({
-  ranking: {flex: 1},
+  ranking: {flex: 1, padding: 24, gap: 24},
+  rankingTitle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  rankingTitleText: {
+    fontSize: 24,
+    fontFamily: 'SUITE-ExtraBold',
+    color: AppColor.brand_white,
+  },
   boxing: {flex: 0},
 });
